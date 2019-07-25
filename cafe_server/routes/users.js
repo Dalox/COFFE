2d8807aea966f30deb6e929d8c7d5769d43e22e5
Dalox <jdn95@gmail.com>
2019-07-24 19:03:55 -0500

    incio del servidor

--- cafe_server/routes/users.js
+++ cafe_server/routes/users.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+const mc = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'coffe'
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/signup', function(req, res, next) {
+  let data = []
+  data.push(req.query.email, req.query.password, req.query.fistname, req.query.lastname)
+  console.log(req.query, data)
+  const sql = 'INSERT INTO `users`(`email`, `password`, `nombre`, `apellido`) VALUES (?,?,?,?)'
+  mc.query(sql,data,(err) => {
+    if(err) {
+      console.error(err)
+    }else{
+      console.log(req.query)
+      res.json(req.query)
+    }
+  })
 });
 
 module.exports = router;
